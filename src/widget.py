bdec5a51dae7f1d02ddc014e73d410d6fd986c9a
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2024-09-17 00:10:13 +0300

    Overal check and Corrections of linters were made

--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,20 @@
-from masks import get_mask_card_number
-from masks import get_mask_account
+from masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(card_or_account_info: str) -> str:
     """Function that takes the string with card name or account
     and gives back masked string of digits and stars."""
     if "счет" in card_or_account_info.lower():
-        result = get_mask_account(card_or_account_info[-20:])
+        result = str(get_mask_account(card_or_account_info[-20:]))
     else:
-        result = get_mask_card_number(card_or_account_info[-16:])
+        result = str(get_mask_card_number(card_or_account_info[-16:]))
     return result
 
 
 def get_date(date_line: str) -> str:
     """Function that turns the line with full date information
     into the date as in the example (day.month.year)."""
-    right_date = date_line[8:10] + "." + date_line[5:7] + "." + date_line[:4]
+    right_date = str(date_line[8:10] + "." + date_line[5:7] + "." + date_line[:4])
     return right_date
 
 
