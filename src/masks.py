59421c04ec856c4458a297968563c1066771a75d
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2024-09-16 21:04:46 +0300

    First commit with masks function H\W 9.1

---
+++ src/masks.py
@@ -0,0 +1,19 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Function that takes card number and turns it into the mask
+    as in example XXXX XX** **** XXXX."""
+    part1 = card_number[:4]
+    part2 = card_number[4:6]
+    part3 = card_number[-4:]
+    mask_card_number = part1 + " " + part2 + "** **** " + part3
+    return mask_card_number
+
+
+def get_mask_account(account_number: str) -> str:
+    """Function that takes account number and turns it into the mask
+    as in example **XXXX."""
+    mask_account = "**" + account_number[-4:]
+    return mask_account
+
+
+print(get_mask_card_number("2136456789874521"))
+print(get_mask_account("21364567898745213789"))
