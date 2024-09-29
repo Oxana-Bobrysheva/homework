3a04bc582c227ff68e73dad5459a85fc3eb15590
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2024-09-29 17:37:15 +0300

    comments has been made

--- src/widget.py
+++ src/widget.py
@@ -18,6 +18,7 @@ def get_date(date_line: str) -> str:
     return right_date
 
 
+# This part is to check if function is working.
 result = mask_account_card("Visa Platinum 7000792289606361")
 print(result)
 print(get_date("2024-03-11T02:26:18.671407"))
