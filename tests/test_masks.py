7153aa42e620c8e5f80b955015b53fc5b0701eaf
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2024-10-03 19:09:40 +0300

    The test for function get_mask_card_number was corrected

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,3 +3,7 @@ from src.masks import get_mask_card_number
 def test_get_mask_card_number():
     assert get_mask_card_number('1234567812345678') == '1234 56** **** 5678'
     assert get_mask_card_number('') == 'This card number is NOT correct. It should have 16 numbers. Try again.'
+    assert get_mask_card_number('123123123') == 'This card number is NOT correct. It should have 16 numbers. Try again.'
+    assert get_mask_card_number('Visa 1231231231231234') == 'This card number is NOT correct. It should have 16 numbers. Try again.'
+
+
