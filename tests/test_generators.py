c736ec7033fac117f38f53e106e6a0054dc4d355
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2024-10-07 04:49:36 +0300

    Two test functions were added in a new module test_generators.py

---
+++ tests/test_generators.py
@@ -0,0 +1,14 @@
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+
+def test_filter_by_currency(list_of_transactions: list) -> None:
+    expected_result = {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572', 'operationAmount':
+        {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from':
+        'Счет 75106830613657916952', 'to': 'Счет 11776614605963066702'}
+    result = filter_by_currency(list_of_transactions, 'USD')
+    assert  next(result) == expected_result
+
+
+def test_transaction_descriptions(list_of_transactions: list) -> None:
+    expected_result = "Перевод организации"
+    result = transaction_descriptions(list_of_transactions)
+    assert  next(result) == expected_result
