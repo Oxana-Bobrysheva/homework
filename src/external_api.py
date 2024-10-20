f1696f6daccfae8cdd0f6afa0580575d939ad15b
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2024-10-20 13:24:57 +0300

    Function exchange_currentcy was written and .env file was added to the package src

---
+++ src/external_api.py
@@ -0,0 +1,24 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def exchange_currentcy(to, from1, amount):
+    """Function that takes rate from API and returns the exchange amount of RUBLES"""
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from1}&amount={amount}"
+
+    headers = {"apikey": os.getenv("API_TOKEN")}
+
+    response = requests.get(url, headers=headers)
+
+    status_code = response.status_code
+    if status_code:
+        result = response.json().get("result")
+    return result
+
+
+if __name__ == "__main__":
+    print(exchange_currentcy("RUB", "EUR", 100))
