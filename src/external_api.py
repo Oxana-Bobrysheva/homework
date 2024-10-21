b24291ef6f644f65d95e46b2e4ac9bcf8b21a228
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2024-10-21 21:51:03 +0300

    Function was corrected

--- src/external_api.py
+++ src/external_api.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import requests
@@ -6,19 +7,30 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def exchange_currency(to, from1, amount):
+def exchange_currency(transaction: dict) -> float:
     """Function that takes rate from API and returns the exchange amount of RUBLES"""
-    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from1}&amount={amount}"
 
-    headers = {"apikey": os.getenv("API_TOKEN")}
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        result = response.json().get("result")
-        return result
+    amount = transaction['operationAmount']['amount']
+    from_currency = transaction['operationAmount']['currency']['code']
+    if from_currency == 'RUB':
+        return float(amount)
     else:
-        return None
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to={"RUB"}&from={from_currency}&amount={amount}"
+
+        headers = {"apikey": os.getenv("API_TOKEN")}
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            result = response.json().get("result")
+            return result
+        else:
+            return None
+
 
 if __name__ == "__main__":
-    print(exchange_currency("RUB", "EUR", 100))
+    print(os.getenv("API_TOKEN"))
+    with open('../data/operations.json', 'r', encoding='UTF-8') as file:
+        operations = json.load(file)
+        for o in operations[:5]:
+            print(exchange_currency(o))
