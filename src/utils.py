d5556b1d7e4414e085daa6515210bb1e84408494
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2024-10-21 23:51:37 +0300

    Logging was added to utils.py, Direcory logs created and file utils.log was excluded form ignored files

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,36 @@
 import json
 import os
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('../logs/utils.log', mode='w')
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
 
 def get_transactions(url: str) -> list[dict]:
     """ Gets data information about transactions from operations.json"""
+    logger.info('Starting the function get_transactions')
     if not os.path.isfile(url):
         # file is not found
+        logging.warning("There is no such file")
         return []
     with open(url, 'r', encoding='UTF-8') as file:
         try:
+            logger.info('Downloading data from file')
             data = json.load(file)
             if isinstance(data, list):
                 return data
             else:
                 # if data is not a list
+                logging.warning('Data is not a list')
                 return []
+
         except (json.JSONDecodeError, UnicodeDecodeError) as e:
             # Error in decoding json or reading file
+            logger.error(f'An Error has occurred {e}')
             print(f"Error reading file: {e}")
             return []
 
