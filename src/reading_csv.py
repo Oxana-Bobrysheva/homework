5899d1f401a2bb1b3f54f12542a47dc1a00f3f56
Oxana-Bobrysheva <bobrishevaoksana77@gmail.com>
2024-10-28 00:12:12 +0300

    Docstrings were added

--- src/reading_csv.py
+++ src/reading_csv.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 
 def reading_csv(file_name):
+    """This function allows to read data from csv files."""
     df = pd.read_csv(file_name)
     return df
 
--- src/reading_excel.py
+++ src/reading_excel.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 
 def reading_xlsx(file_name):
+    """This function allows to read data from Excel files."""
     df = pd.read_excel(file_name)
     return df
 
