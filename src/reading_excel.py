7eb98c6de426fbf2523611ea32da6db774e109b2
Oxana-Bobrysheva <bobrishevaoksana77@gmail.com>
2024-10-28 11:46:57 +0300

    Functions fixed to give out list of dictionaries

--- src/reading_excel.py
+++ src/reading_excel.py
@@ -2,11 +2,20 @@ import pandas as pd
 
 
 def reading_xlsx(file_name):
-    """This function allows to read data from Excel files."""
-    df = pd.read_excel(file_name)
-    return df
+    """This function allows to read data from Excel files and returns a list of
+    dictionaries."""
+    transactions = []
+    try:
+        df = pd.read_excel(file_name)
+        for index, row in df.iterrows():
+            transactions.append(row.to_dict())
+    except FileNotFoundError:
+        print(f"File {file_name} was not found.")
+    except Exception as e:
+        print(f"Error occurred: {e}.")
+    return transactions
 
 
 if __name__ == "__main__":
     result = reading_xlsx("../data/transactions_excel.xlsx")
-    print(result.head())
+    print(result[:3])
